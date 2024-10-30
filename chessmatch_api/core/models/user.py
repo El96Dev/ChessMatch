844e18ac58708ccbe9da2bfdb6fb8481be4159f2
EL96cpp <el96cpp@gmail.com>
2024-10-30 12:41:18 +0300

    Added get_db classmethod to the User model

--- chessmatch_api/core/models/user.py
+++ chessmatch_api/core/models/user.py
@@ -1,11 +1,18 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
+from typing import TYPE_CHECKING
 
 from .base import Base
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     username: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
     elo_rating: Mapped[int] = mapped_column(default=0) 
     avatar_filename: Mapped[str] = mapped_column(default="default.jpg")
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
